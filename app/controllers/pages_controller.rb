617355715b6dbd1915f953b21ab0ae3fcb6ae6c5
Lucas Caton <lucascaton@gmail.com>
2013-05-03 15:53:31 -0300

    Redis pub/sub

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,30 @@ class PagesController < ApplicationController
   include ActionController::Live
 
   def index
-    response.headers['Content-Type'] = 'Text/event-strem'
+    response.headers['Content-Type'] = 'text/event-stream'
 
-    5.times do |n|
+    3.times do |n|
       response.stream.write "Hello, browser! #{n + 1}\n"
       sleep 1
     end
 
     response.stream.close
+  end
+
+  def index2
+  end
+
+  def items
+    response.headers['Content-Type'] = 'text/event-stream'
+
+    redis = Redis.new
+
+    redis.subscribe('pages.items') do |on|
+      on.message do |event, data|
+        response.stream.write("data: #{data}\n\n")
+      end
+    end
+
+    response.strem.close
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 ActionControllerLiveExample::Application.routes.draw do
   root to: 'pages#index'
+
+  get 'index2' => 'pages#index2'
+  get 'items' => 'pages#items'
 end
