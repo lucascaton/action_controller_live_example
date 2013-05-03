ca24179ed78f3694c2f17656362300cebec73a7a
Lucas Caton <lucascaton@gmail.com>
2013-05-03 00:49:04 -0300

    Initial Rails directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActionControllerLiveExample::Application.config.secret_key_base = '723a39295bc81478a3cd26b0bc760a1c2451ad3026183380ae1be7bd5ec874753185918f3ae986cd99ac57abb596eb2889c4a71000ba1e32e262b07f2b91e971'
